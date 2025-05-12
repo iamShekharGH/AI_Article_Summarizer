de794e5ac668deba29995447ca8eeb57d4e13ffd
iamShekharGH <iamShekharGH@users.noreply.github.com>
2025-05-12 17:54:26 +0530

    feat: Add Error and Loading UI components
    Introduces dedicated composable functions for displaying loading and error states in the UI.
    
    - Creates `ErrorUI.kt` with a basic error message and icon.
    - Creates `LoadingUI.kt` with a circular progress indicator and "Loading..." text.
    - Both components are centered and occupy the full screen space.

---
+++ app/src/main/java/com/shekharhandigol/aiarticlesummarizer/ui/common/ErrorUI.kt
@@ -0,0 +1,46 @@
+package com.shekharhandigol.aiarticlesummarizer.ui.common
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun ErrorUi() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Icon(
+            imageVector = Icons.Filled.Warning,
+            contentDescription = "Error Loading",
+            modifier = Modifier
+                .size(100.dp)
+                .padding(16.dp)
+        )
+        Text(
+            text = "Error Loading Articles",
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 21.sp
+        )
+
+    }
+}
---
+++ app/src/main/java/com/shekharhandigol/aiarticlesummarizer/ui/common/LoadingUI.kt
@@ -0,0 +1,44 @@
+package com.shekharhandigol.aiarticlesummarizer.ui.common
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LoadingUi() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        CircularProgressIndicator(
+            modifier = Modifier
+                .size(100.dp)
+                .padding(16.dp),
+        )
+
+        Text(
+            text = "Loading...",
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 21.sp
+        )
+
+    }
+}
