8ae996e3558e03562478054826c65893d5e10a47
iamShekharGH <iamShekharGH@users.noreply.github.com>
2025-06-02 17:32:45 +0530

    refactor: Use MaterialTheme typography for text styles
    This commit updates the `ErrorUI` and `LoadingUI` composables to use `MaterialTheme.typography` for text styling instead of hardcoded font sizes.
    
    - In `ErrorUI.kt`, the error message text now uses `MaterialTheme.typography.headlineSmall`.
    - In `LoadingUI.kt`, the "Loading..." text now uses `MaterialTheme.typography.titleLarge`.
    
    Signed-off-by: iamShekharGH <iamShekharGH@users.noreply.github.com>

--- app/src/main/java/com/shekharhandigol/aiarticlesummarizer/ui/common/LoadingUI.kt
+++ app/src/main/java/com/shekharhandigol/aiarticlesummarizer/ui/common/LoadingUI.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -37,7 +37,7 @@ fun LoadingUi() {
             text = "Loading...",
             modifier = Modifier.padding(16.dp),
             textAlign = TextAlign.Center,
-            fontSize = 21.sp
+            style = MaterialTheme.typography.titleLarge
         )
 
     }
